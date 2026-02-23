24259362d5d7731a8aab6db3a871ced4cef763f0
Carter Burtlake <carterburtlake@Carters2025-MacBook-Air-2.local>
2026-02-23 10:16:39 -0800

    Add code from Em's project to copy general scaffolding

--- RLSdata/RLSdata.R
+++ RLSdata/RLSdata.R
@@ -1,1 +1,26 @@
-read.csv("RLSdata/RLSBarkelySound_Inverts2021-2024.csv")
+# RLS abalone data
+# Carter Burtlake 
+# February 2026
+
+# Load packages and data -----
+# Load packages
+library(readr)
+library(tidyr)
+library(dplyr) # for general data wrangling
+library(lubridate) # for looking at dates
+
+library(glmmTMB) # The swiss army knife of modeling packages
+library(DHARMa) # inspect model residuals/check assumptions
+library(ggeffects) # for extracting predictions and running post hoc tests
+
+library(ggplot2) # Plotting data
+library(patchwork) # Arrange multiple plots together
+library(visreg) # plot model predictions
+library(viridisLite) # colour palettes
+
+library(ggspatial)
+library(sf)
+
+# Load and manipulate data -----------------------------------------------------
+abalone <- read_csv("RLSdata/RLSBarkelySound_Inverts2021-2024.csv") # this is the averaged abalone data from RLS
+
